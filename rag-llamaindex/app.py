ea963d7c68bcbab13bae4eccc2cc6f58c89ff972
Wendell Liu <cuk.bas@gmail.com>
2025-05-07 18:56:09 +0800

    feat: Conversation Memory

--- rag-llamaindex/app.py
+++ rag-llamaindex/app.py
@@ -1,6 +1,8 @@
 import asyncio
 from llama_index.core.agent.workflow import AgentWorkflow
 from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
+from llama_index.core.workflow import Context
+
 
 from retriever import guest_info_tool
 from tools.search_tool import search_tool
@@ -20,12 +22,20 @@ alfred = AgentWorkflow.from_tools_or_functions(
 
 
 async def main():
-    # Run the agent with a sample query
-    # response = await alfred.run("Tell me about our guest named 'Lady Ada Lovelace'.")
-    # response = await alfred.run("Who's the current President of France?")
-    response = await alfred.run("What is the weather tomorrow in Paris?")
-    print("🎩 Alfred's Response:")
-    print(response)
+    ctx = Context(alfred)
+    # response1 = await alfred.run("Tell me about Lady Ada Lovelace.", ctx=ctx)
+    # print("🎩 Alfred's First Response:")
+    # print(response1)
+    #
+    # response2 = await alfred.run("What projects is she currently working on?", ctx=ctx)
+    # print("🎩 Alfred's Second Response:")
+    # print(response2)
+
+    response3 = await alfred.run(
+        "Do you think it's good day to go outside tomorrow in New York?", ctx=ctx
+    )
+    print("🎩 Alfred's Third Response:")
+    print(response3)
 
 
 asyncio.run(main())
